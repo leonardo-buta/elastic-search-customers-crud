1ca903f42ec0d606ae4948ebf5b03d8890010695
Leonardo Buta <leonardo.lbuta@gmail.com>
2020-07-12 00:01:17 -0300

    Added Model class

---
+++ src/ElasticSearch.Customers.CRUD/Models/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace ElasticSearch.Customers.CRUD.Models
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ src/ElasticSearch.Customers.CRUD/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ElasticSearch.Customers.CRUD.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Address Address { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
